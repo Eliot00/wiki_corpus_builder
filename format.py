fabeb06ee77da77343274125fa26579ddfab5ac8
Elliot <elliot.xu@coherencedigital.com>
2023-04-06 12:33:11 +0800

    fix: correct num to chinese

--- format.py
+++ format.py
@@ -3,7 +3,7 @@ import re
 import pypinyin
 from tqdm import tqdm
 import jieba
-from pycnnum import num2cn
+import itertools
 
 digit_pattern = re.compile(r'(?<=[\u4e00-\u9fff])\d+\.?\d*|\d*\.?\d+(?=[\u4e00-\u9fff%])')
 percent_pattern = re.compile(r'(?<=[\u4e00-\u9fff])\d+\.?\d*%')
@@ -16,20 +16,19 @@ def convert_digit_to_chinese(sentence: str) -> str:
     percent_matches = percent_pattern.findall(sentence)
     for match in percent_matches:
         num = float(match[:-1])
-        cn_num = num2cn(str(num), traditional=True, alt_two=True, alt_zero=True)
-        sentence = sentence.replace(match, '百分之' + cn_num)
+        try:
+            cn_num = num2chinese(num, simp=False, o=True, twoalt=True)
+            sentence = sentence.replace(match, '百分之' + cn_num)
+        except Exception:
+            print(f"match: {match} sentence: {sentence}")
 
     matches = digit_pattern.findall(sentence)
     for match in matches:
-        if '.' in match:  # 匹配到小数
-            cn_num = num2cn(match, traditional=True, alt_two=True, alt_zero=True)
+        try:
+            cn_num = num2chinese(match, simp=False, o=True, twoalt=True)
             sentence = sentence.replace(match, cn_num)
-        # pycnnum遇到1会出现IndexError，临时处理一下
-        elif match == '1':
-            sentence = sentence.replace(match, '一')
-        else:  # 匹配到整数
-            cn_num = num2cn(match, traditional=True, alt_two=True, alt_zero=True)
-            sentence = sentence.replace(match, cn_num)
+        except Exception:
+            print(f"match: {match} sentence: {sentence}")
     return sentence
 
 year_digit_map = {
@@ -64,6 +63,90 @@ def year_special_convert(sentence: str) -> str:
         sentence = sentence.replace(match, cn_year + '年')
     return sentence
 
+# https://gist.github.com/gumblex/0d65cad2ba607fd14de7
+def num2chinese(num, big=False, simp=True, o=False, twoalt=False):
+    """
+    Converts numbers to Chinese representations.
+    `big`   : use financial characters.
+    `simp`  : use simplified characters instead of traditional characters.
+    `o`     : use 〇 for zero.
+    `twoalt`: use 两/兩 for two when appropriate.
+    Note that `o` and `twoalt` is ignored when `big` is used, 
+    and `twoalt` is ignored when `o` is used for formal representations.
+    """
+    # check num first
+    nd = str(num)
+    if abs(float(nd)) >= 1e48:
+        raise ValueError('number out of range')
+    elif 'e' in nd:
+        raise ValueError('scientific notation is not supported')
+    c_symbol = '正负点' if simp else '正負點'
+    if o:  # formal
+        twoalt = False
+    if big:
+        c_basic = '零壹贰叁肆伍陆柒捌玖' if simp else '零壹貳參肆伍陸柒捌玖'
+        c_unit1 = '拾佰仟'
+        c_twoalt = '贰' if simp else '貳'
+    else:
+        c_basic = '〇一二三四五六七八九' if o else '零一二三四五六七八九'
+        c_unit1 = '十百千'
+        if twoalt:
+            c_twoalt = '两' if simp else '兩'
+        else:
+            c_twoalt = '二'
+    c_unit2 = '万亿兆京垓秭穰沟涧正载' if simp else '萬億兆京垓秭穰溝澗正載'
+    revuniq = lambda l: ''.join(k for k, g in itertools.groupby(reversed(l)))
+    nd = str(num)
+    result = []
+    if nd[0] == '+':
+        result.append(c_symbol[0])
+    elif nd[0] == '-':
+        result.append(c_symbol[1])
+    if '.' in nd:
+        integer, remainder = nd.lstrip('+-').split('.')
+    else:
+        integer, remainder = nd.lstrip('+-'), None
+    if integer and int(integer):
+        splitted = [integer[max(i - 4, 0):i]
+                    for i in range(len(integer), 0, -4)]
+        intresult = []
+        for nu, unit in enumerate(splitted):
+            # special cases
+            if int(unit) == 0:  # 0000
+                intresult.append(c_basic[0])
+                continue
+            elif nu > 0 and int(unit) == 2:  # 0002
+                intresult.append(c_twoalt + c_unit2[nu - 1])
+                continue
+            ulist = []
+            unit = unit.zfill(4)
+            for nc, ch in enumerate(reversed(unit)):
+                if ch == '0':
+                    if ulist:  # ???0
+                        ulist.append(c_basic[0])
+                elif nc == 0:
+                    ulist.append(c_basic[int(ch)])
+                elif nc == 1 and ch == '1' and unit[1] == '0':
+                    # special case for tens
+                    # edit the 'elif' if you don't like
+                    # 十四, 三千零十四, 三千三百一十四
+                    ulist.append(c_unit1[0])
+                elif nc > 1 and ch == '2':
+                    ulist.append(c_twoalt + c_unit1[nc - 1])
+                else:
+                    ulist.append(c_basic[int(ch)] + c_unit1[nc - 1])
+            ustr = revuniq(ulist)
+            if nu == 0:
+                intresult.append(ustr)
+            else:
+                intresult.append(ustr + c_unit2[nu - 1])
+        result.append(revuniq(intresult).strip(c_basic[0]))
+    else:
+        result.append(c_basic[0])
+    if remainder:
+        result.append(c_symbol[2])
+        result.append(''.join(c_basic[int(ch)] for ch in remainder))
+    return ''.join(result)
 def get_words_and_pinyins(sentence: str) -> tuple[str, str]:
     words = jieba.cut(sentence, cut_all=False)
 
